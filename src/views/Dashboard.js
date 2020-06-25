6698c969d742d558ddb07d91711cd2c02a2f973b
seakmengc <c.seakmeng0603@gmail.com>
2020-06-25 17:31:21 +0700

    add: dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -26,6 +26,7 @@ import {
   Col,
   UncontrolledTooltip,
   Spinner,
+  Pagination,
 } from 'reactstrap'
 
 import { AuthContext } from 'contexts/auth'
@@ -33,45 +34,14 @@ import { useApolloClient, useQuery } from 'react-apollo'
 import { MY_FORUMS_QUERY } from 'constants/forum'
 import { MY_COMMENTS_QUERY } from 'constants/forum'
 import { CLASSES_QUERY } from 'constants/class'
+import MyForums from '../components/Cards/MyForums'
+import MyComments from 'components/Cards/MyComments'
 
 const Dashboard = (props) => {
   const authContext = useContext(AuthContext)
   const client = useApolloClient()
-  const [myForums, setMyForums] = useState(null)
-  const [myComments, setMyComments] = useState(null)
-  const [classes, setClasses] = useState(null)
 
   useEffect(() => {
-    async function myForumsQuery() {
-      try {
-        const res = await client.query({
-          query: MY_FORUMS_QUERY,
-          variables: {
-            first: 10,
-            page: 1,
-          },
-        })
-        return res.data.myForums
-      } catch (e) {
-        console.log(e)
-      }
-    }
-
-    async function myCommentsQuery() {
-      try {
-        const res = await client.query({
-          query: MY_COMMENTS_QUERY,
-          variables: {
-            first: 10,
-            page: 1,
-          },
-        })
-        return res.data.myComments
-      } catch (e) {
-        console.log(e)
-      }
-    }
-
     async function getClasses() {
       try {
         const res = await client.query({
@@ -83,31 +53,15 @@ const Dashboard = (props) => {
         })
 
         return res.data.classes
-      }catch (e) {
+      } catch (e) {
         console.log(e)
       }
-    }
-
-    myForumsQuery().then((forums) => {
-      setMyForums(forums)
-    })
-
-    myCommentsQuery().then((comments) => {
-      setMyComments(comments)
-      console.log(comments)
-    })
-
-    if(authContext.users.roles[0].name === 'teacher') {
-      setClasses(authContext.users.teachings);
-    } else if(authContext.users.roles[0].name === 'student') {
-      setClasses(authContext.users.learnings);
     }
   }, [])
 
   const renderClasses = (classes) => {
-    const zeroPad = (num, places) => String(num).padStart(places, '0')
-    if(!classes) return <Spinner />
-    
+    if (!classes) return <Spinner />
+
     return classes.map((class_) => {
       return (
         <tr key={class_.id}>
@@ -115,31 +69,30 @@ const Dashboard = (props) => {
             <a href="#">{class_.name}</a>
           </td>
           <td>{class_.code}</td>
-          <td>
-            {class_.schedules.map((schedule) => {
-              return (
-                <Row>
-                  {console.log(schedule)}
-                  <Col lg="6" md="12" className="text-strong">
-                    {schedule.day}
-                  </Col>
-                  <Col lg="6" md="12">
-                    {schedule.sessions &&
-                      schedule.sessions.map((session) => {
-                        return (
-                          <tr>
-                            <td>{
-                                zeroPad(session.start_time, 4).slice(0, 2) + ':' + zeroPad(session.start_time, 4).slice(2)
-                              }</td>
-                            <td>-</td>
-                            <td>{zeroPad(session.end_time, 4).slice(0, 2) + ':' + zeroPad(session.end_time, 4).slice(2)}</td>
-                          </tr>
-                        )
-                      })}
-                  </Col>
-                </Row>
-              )
-            })}
+          <td width="min-content">
+            <Table striped responsive>
+              {class_.schedules.map((schedule) => {
+                return (
+                  <Row className="p-2">
+                    <Col lg="6" md="12" className="text-left">
+                      {schedule.day}
+                    </Col>
+                    <Col lg="6" md="12">
+                      {schedule.sessions &&
+                        schedule.sessions.map((session) => {
+                          return (
+                            <tr>
+                              <td>{session.start_time}</td>
+                              <td>-</td>
+                              <td>{session.end_time}</td>
+                            </tr>
+                          )
+                        })}
+                    </Col>
+                  </Row>
+                )
+              })}
+            </Table>
           </td>
         </tr>
       )
@@ -159,81 +112,14 @@ const Dashboard = (props) => {
                 <tr>
                   <th>Name</th>
                   <th>Code</th>
-                  <th>Schedule</th>
+                  <th className="text-center">Schedule</th>
                 </tr>
               </thead>
               <tbody>
-                {/* {authContext.user.learnings &&
+                {authContext.user.learnings &&
                   renderClasses(authContext.user.learnings)}
                 {authContext.user.teachings &&
-                renderClasses(authContext.user.teachings)} */}
-                {renderClasses(classes)}
-              </tbody>
-            </Table>
-          </CardBody>
-        </Card>
-      </Col>
-    ),
-    myForums: (
-      <Col lg="6" md="12">
-        <Card>
-          <CardHeader>
-            <CardTitle tag="h4">Recent Forums</CardTitle>
-          </CardHeader>
-          <CardBody>
-            <Table className="tablesorter">
-              <thead className="text-primary">
-                <tr>
-                  <th>Title</th>
-                  <th>Comment Count</th>
-                </tr>
-              </thead>
-              <tbody>
-                {myForums &&
-                  myForums.data.map((forum) => {
-                    return (
-                      <tr>
-                        <td>
-                          <a href="#">{forum.title}</a>
-                        </td>
-                        <td>{forum.comments_count}</td>
-                      </tr>
-                    )
-                  })}
-              </tbody>
-            </Table>
-          </CardBody>
-        </Card>
-      </Col>
-    ),
-    myComments: (
-      <Col lg="6" md="12">
-        <Card>
-          <CardHeader>
-            <CardTitle tag="h4">Recent Comments</CardTitle>
-          </CardHeader>
-          <CardBody>
-            <Table className="tablesorter">
-              <thead className="text-primary">
-                <tr>
-                  <th>Comment</th>
-                  <th>Forum</th>
-                </tr>
-              </thead>
-              <tbody>
-                {myComments &&
-                  myComments.data.map((comment) => {
-                    return (
-                      <tr>
-                        <td>
-                          <a href="#">{comment.comment}</a>
-                        </td>
-                        <td>
-                          <a href="#">{comment.commentable.title}</a>
-                        </td>
-                      </tr>
-                    )
-                  })}
+                  renderClasses(authContext.user.teachings)}
               </tbody>
             </Table>
           </CardBody>
@@ -243,14 +129,15 @@ const Dashboard = (props) => {
   }
 
   return (
-    <>
-      <div className="content">
-        <Row>{ele.classes}</Row>
-        <Row>
-          {ele.myForums} {ele.myComments}
-        </Row>
-      </div>
-    </>
+    <div className="content">
+      <Row>
+        <MyForums />
+        <MyComments />
+      </Row>
+      <Row>
+        {ele.classes}
+      </Row>
+    </div>
   )
 }
 
