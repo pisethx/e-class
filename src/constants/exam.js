6ce23f9b829cf1a1a1bfe9511703a1c6953a7659
seakmengc <c.seakmeng0603@gmail.com>
2020-07-09 01:29:36 +0700

    add: student takes exam related query and mutation

--- src/constants/exam.js
+++ src/constants/exam.js
@@ -28,3 +28,94 @@ export const CREATE_EXAM_MUTATION = gql`
     }
   }
 `
+
+export const STUDENT_EXAM_IDS_QUERY = gql`
+  mutation STUDENT_EXAM_IDS_QUERY(
+    $class_id: ID!
+  ) {
+    studentExamIds(id: $class_id)
+  }
+`
+
+export const GRADE_STUDENT_EXAM_MUTATION = gql`
+  mutation GRADE_STUDENT_EXAM_MUTATION(
+    $id: ID!
+    $answer: [GradeExamAnswerInput]
+  ) {
+    gradeStudentExam(input: {
+      id: $id,
+      answer: $answer
+    }) {
+      id
+    }
+  }
+`
+
+export const MY_EXAMS_IN_CLASS_QUERY = gql`
+  query MY_EXAMS_IN_CLASS_QUERY(
+    $class_id: ID!
+  ) {
+    myExamsInClass(class_id: $class_id) {
+      id
+      answer {
+        answers
+        file {
+          url
+          name
+        }
+        points
+      }
+      points
+      attempts
+      created_at
+   }
+  }
+`
+
+export const STUDENT_EXAM_QUERY = gql`
+  query STUDENT_EXAM_QUERY(
+    $id: ID!
+  ) {
+    studentExam(id: $id) {
+      id
+      answer {
+        answers
+        file {
+          url
+          name
+        }
+        points
+      }
+      points
+      attempts
+      created_at
+   }
+  }
+`
+
+export const STUDENT_TAKES_EXAM_MUTATION = gql`
+  mutation STUDENT_TAKES_EXAM_MUTATION(
+    $exam_id: ID!
+    $answers: [CreateExamAnswerInput]
+  ) {
+    studentTakesExam(
+      input: {
+        exam_id: $exam_id,
+        answer: $answers
+      }
+    ) {
+      id
+      attempts
+      answer {
+        id
+        answers
+        file {
+          url
+          name
+        }
+        points
+      }
+      points
+    }
+  }
+`
