56b2b3fc5649622eb4829314e857f9c09ab5d08e
Monespiseth Ly <piseth@MacBook-Pro.local>
2020-07-14 00:27:41 +0700

    clean up

--- src/context/UserContext.js
+++
@@ -1,5 +0,0 @@
-// import React, { createContext } from 'react'
-// import { useQuery } from 'react-apollo'
-// import { CURRENT_USER_QUERY } from './Api'
-
-// export const UserContext = createContext(null)
